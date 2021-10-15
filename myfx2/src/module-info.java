ccb9aa2c1814c3bc71cd7190fd06fce3323c2e20
user <user@DESKTOP-CTPDP9V>
2021-10-15 11:12:09 +0900

    dB Handling

---
+++ myfx2/src/module-info.java
@@ -0,0 +1,9 @@
+module myfx2 {
+	requires javafx.controls;
+	requires javafx.graphics;
+	requires javafx.fxml;
+	requires java.sql;
+	requires java.desktop;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
